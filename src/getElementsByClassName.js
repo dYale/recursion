9492ba183f21f95861f9922678398b5aa02543de
Dan Corman <dan.corman@gmail.com>
2015-04-17 19:22:06 -0500

    GEBCN developed, working on right formatting

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,22 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-){
-  // your code here
-};
+var getElementsByClassName = function(className){
+
+    
+var nodeList = [];
+
+ function hasClass(node){
+for(var i = 0; i < node.childNodes.length; i++){
+    if(node.childNodes[i].classList){
+     if(node.childNodes[i].classList.contains(className)){
+     nodeList.push(node.childNodes[i]);
+ } 
+    }if(node.hasChildNodes());
+    hasClass(node.childNodes[i])
+    }
+    }
+    hasClass(document.body)
+    console.log(nodeList);
+return nodeList;
+}
